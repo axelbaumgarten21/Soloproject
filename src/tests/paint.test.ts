5278959d747580727e81c4b995de9797043228ff
axelbaumgarten21 <axelbaumgarten21@gmail.com>
2021-08-26 21:22:57 -0500

    updates

--- src/tests/paint.test.ts
+++ src/tests/paint.test.ts
@@ -1,27 +1,69 @@
 import { source } from "./source"
+import { Builder, Capabilities, By, ThenableWebDriver, Actions, WebElement, WebDriver, Locator, Condition, TouchSequence, } from "selenium-webdriver"
+import { del } from "selenium-webdriver/http";
 
-import { Builder, Capabilities, By, ThenableWebDriver, Actions, WebElement, WebDriver, Locator, Condition } from "selenium-webdriver"
+
 
 const chromedriver = require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 const page = new source(driver);//this is to utilize my source page functions
 
+const colornumber: By = By.xpath("//*[@id='rc-editable-input-7']");
+
+const actions = driver.actions({async: true});
+const kb = actions.keyboard();
+const mouse = actions.mouse();
+const coloredit: By = By.xpath("//*[@id='rc-editable-input-7']");
+const subscribebutton: By = By.xpath("//*[@id='ele44']");
+const signinbutton: By = By.xpath("//*[@class='signin-btn']");
+
+
 
 beforeAll(async () => {
-    await driver.get('https://sumo.app/paint/');
+    await driver.get('https://www.sumopaint.com/');
 });
 
 afterAll(async () => {
     await (await driver).quit()
 })
-jest.setTimeout(300000)
+jest.setTimeout(40000)
 
 
-test('Simple Fill', async () => {
-    driver.manage().window().maximize();
-    await page.filltest();
-    await driver.sleep(5000)
-});
-test("using fill and paintbrush", async () => {
+// test('Simple Fill', async () => {
+    // driver.manage().window().maximize();
+    // await page.gotoapp();
+    // await page.selectcolor();
+    // driver.findElement(colornumber).click();
+    // actions.press().release().press().release();
 
+    // await driver.sleep(5000)
+    // driver.findElement(colornumber).click;
+    // await driver.findElement(colornumber).isSelected();
+    // driver.findElement(colornumber);
+    
+    // driver.findElement(colornumber).clear();
+
+    // driver.findElement(colornumber).clear();
+    // page.typecolor("A10678");
+//  actions.perform();
+    // await page.redfill();
+    // await driver.sleep(2000);
+// });
+// test("using fill and paintbrush", async () => {
+    // driver.manage().window().maximize();
+    // await page.gotoapp();
+    // await page.filltest();
+    // await page.usebrush();
+    // await driver.sleep(5000);
+
+// });
+test("login to account", async  () => {
+    await driver.manage().window().maximize();
+// String parentwindow = driver.getWindowHandle();
+    await driver.findElement(subscribebutton).click();
+    await driver.sleep(5000);
+    await page.clicky();
+// String childWindow = driver.getWindowHandle();
+//      await driver.switchTo().window(childWindow);
+    await driver.findElement(signinbutton).click();
 });
--- src/tests/source.ts
+++ src/tests/source.ts
@@ -1,17 +1,28 @@
-import { By, until, WebDriver } from "selenium-webdriver";
+import { Browser, By, Key, until, WebDriver } from "selenium-webdriver";
+
+
+
+
 
 
     export class source {
         driver: WebDriver;
         url: string = "https://sumo.app/paint/";
 
-
 fillbutton: By = By.xpath("//*[@id='tool-9']");
 paintbrush: By = By.xpath("//*[@id='tool-5']");
 canvas: By = By.xpath("//*[@id='canvasWrapper']");
 color: By = By.xpath("//*[@id='toolsNavWrapper']/div[1]/div[1]/div/div");
 colornumber: By = By.xpath("//*[@id='rc-editable-input-7']");
-colorokbutton: By = By.xpath("//*[@id='root']/div/div/div[2]/div[5]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]")
+colorokbutton: By = By.xpath("//*[@id='root']/div/div/div[2]/div[5]/div[1]/div/div[2]/div[3]/div/div[2]/div[1]");
+paintonlinebutton: By = By.xpath("//*[@id='ele33']");
+clickout: By = By.xpath("//*[@id='install-pwa-background']");
+colorbar: By = By.xpath("//*[@id='root']/div/div/div[2]/div[5]/div[1]/div/div[2]/div[2]/div/div");
+colorbox: By = By.xpath("//*[@id='root']/div/div/div[2]/div[5]/div[1]/div/div[2]/div[1]/div/div/div[2]/div");
+red: By = By.xpath("//*[@id='rc-editable-input-4']");
+username: By = By.xpath("//*[@id='loginusername-input']");
+signinbutton: By = By.xpath("//*[@class='signin-btn']");
+
 
         
 
@@ -26,20 +37,48 @@ async getText(elementBy: By) {
     await this.driver.wait(until.elementLocated(elementBy));
     return (await this.driver.findElement(elementBy)).getText();
   }
-async filltest() {
+
+async selectcolor() {
   await this.driver.wait(until.elementLocated(this.color)).click();
-  await this.driver.wait(until.elementLocated(this.colornumber)).click();
-  await this.sendKeys(this.colornumber, 25325);
+  // await this.driver.wait(until.elementLocated(this.colornumber));
+} 
+async redfill() {
+  await this.sendKeys(this.colornumber, "A60606");
   await this.driver.wait(until.elementLocated(this.colorokbutton)).click();
   await this.driver.wait(until.elementLocated(this.fillbutton)).click();
   await this.driver.wait(until.elementLocated(this.canvas)).click();
 }
+async typecolor(text: string) {
+return this.sendKeys(this.colornumber, `${text}`)
+}
+async clicky() {
+  await this.driver.wait(until.elementLocated(this.signinbutton)).click();
+}
+
+// async filltest() {
+//   await this.driver.wait(until.elementLocated(this.color)).click();
+//   await this.driver.wait(until.elementLocated(this.colornumber)).click();
+//   await this.sendKeys(this.red, 100);
+//   await this.driver.sleep(2000)
+//   await this.driver.wait(until.elementLocated(this.colorokbutton)).click();
+//   await this.driver.wait(until.elementLocated(this.fillbutton)).click();
+//   await this.driver.wait(until.elementLocated(this.canvas)).click();
+// }
 async usebrush() {
   await this.driver.wait(until.elementLocated(this.paintbrush)).click();
-  await this.driver.wait(until.elementLocated(this.colornumber)).click();
-  await this.sendKeys(this.colornumber, 745632);
+  await this.driver.wait(until.elementLocated(this.color)).click();
+  await this.driver.wait(until.elementLocated(this.red)).click();
+  await this.sendKeys(this.colornumber, "0000");
   await this.driver.wait(until.elementLocated(this.colorokbutton)).click();
   await this.driver.wait(until.elementLocated(this.canvas)).click()
+}
+async gotoapp() {
+  await this.driver.wait(until.elementLocated(this.paintonlinebutton)).click();
+  // await this.driver.wait(until.elementLocated(this.clickout)).click();
+
+}
+async typeusername(text: string) {
+  return this.sendKeys(this.username, `${text}`);
 }
 // async navigate() {
 //   await this.driver.get(this.url);
@@ -59,4 +98,5 @@ async usebrush() {
   // async getsearchresults() {
   //   return this.getText(this.searchresults)
   // }
+  
 }
